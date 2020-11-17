ac3ff0ba041a6949adc14fac37f7595ff1908110
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-11-17 01:13:44 -0300

    Adiciona rotas de inicio, profile e extrato

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import LoginScreen from './pages/LoginScreen';
+import Launchpage from './pages/Launchpage';
+import Profile from './pages/Profile';
+import Statement from './pages/Statement';
 
 export default function Routes() {
   const { Navigator, Screen } = createStackNavigator();
@@ -10,6 +14,9 @@ export default function Routes() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name='Login' component={LoginScreen} />
+        <Screen name='Launchpage' component={Launchpage} />
+        <Screen name='Profile' component={Profile} />
+        <Screen name='Statement' component={Statement} />
       </Navigator>
     </NavigationContainer>
   );
